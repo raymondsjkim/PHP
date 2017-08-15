8190cba922c3086affaa850228f27bb0d17ab0a6
Raymond Kim <raymond.sj.kim@gmail.com>
2017-08-14 19:46:12 -0700

    Create forEachLoop.php

---
+++ public/forEachLoop.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $users = array("fullname"=>"Ray Kim", "country"=>"United States", "city"=>"CA", "age"=>28);
+    
+    // For Each Loop example
+    foreach($users as $user) {
+        echo "$user <br>";
+    }    
+
+?>
